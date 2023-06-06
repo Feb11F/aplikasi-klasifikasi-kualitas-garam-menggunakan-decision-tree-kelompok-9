e2e10fde4ea6c5b9398356d34c3739336eaff978
Feb11F <101113614+Feb11F@users.noreply.github.com>
2023-06-06 23:19:11 +0700

    Update bisnis.py

--- bisnis.py
+++ bisnis.py
@@ -20,7 +20,7 @@ st.set_page_config(
     }
 )
 st.write("""
-<center><h2 style = "text-align: justify;">KLASIFIKASI KUALITAS GARAM MENGGUNAKAN DECISION TREE </h2></center>
+<center><h2 style = "text-align: CENTER;">KLASIFIKASI KUALITAS GARAM MENGGUNAKAN DECISION TREE </h2></center>
 """,unsafe_allow_html=True)
 st.write("### Dosen Pengampu : Dr. FIKA HASTARITA RACHMAN, ST., M.Eng",unsafe_allow_html=True)
 
