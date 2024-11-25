ae43aaba0b8c330e02cd933ff4f6d0ce2966b383
Feb11F <101113614+Feb11F@users.noreply.github.com>
2024-11-26 01:31:14 +0700

    Update bisnis.py

--- bisnis.py
+++ bisnis.py
@@ -79,12 +79,15 @@ with st.container():
             # Mengubah fitur menjadi format yang sesuai dengan model SVM (menggunakan dictionary seperti yang diinginkan)
             # Membuat dictionary dengan nama feature yang mengikuti format yang diberikan
             new_data_features = {f"feature_{j}": new_X[0][j] for j in range(new_X.shape[1])}
+            submit = st.form_submit_button("submit")
             if submit:
                 st.subheader('Hasil Prediksi')
             # Menampilkan hasil prediksi
-                st.write('Decision Tree')
-                st.success(input_pred[0])
-                st.write("Akurasi: {:.2f}%".format(accuracy*100))
+            # Prediksi menggunakan model yang dimuat
+            new_pred = loaded_model.classify(new_data_features)
+            
+            # Menampilkan hasil prediksi
+            print(f"Prediction for New Data: {new_pred[0]}")
 
 
         
